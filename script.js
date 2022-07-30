6252ad1c7330a44c0fa56338d85e23aba36bf8f8
Gabriel M <gabriellima19072004@gmail.com>
2022-07-30 14:04:04 -0300

    introdução do javascript

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function IFM(){
     var ensino = selector.options[selector.selectedIndex].value;
 
     if (ensino == 'infantil'){
-        location.href = 'https://pt.wikipedia.org/wiki/Elon_Musk';
+        location.href = 'file:///E:/Projetos/SIME/documentos_infantil.html#';
     }
 
     if (ensino == 'ensino-fundamental'){
-        location.href = 'https://pt.wikipedia.org/wiki/Bill_Gates';
+        location.href = 'file:///E:/Projetos/SIME/documentos_fundamental.html#';
     }
 
     if (ensino == 'ensino-medio'){
-        location.href = 'https://pt.wikipedia.org/wiki/Steve_Jobs';
+        location.href = 'file:///E:/Projetos/SIME/documentos_medio.html#';
         }
 
 }
